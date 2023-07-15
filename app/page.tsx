fe431a2c6c159acdb029a582640beb196404c49f
chasechristensen1 <chase.christensen@inmoment.com>
2023-07-15 13:54:43 -0600

    add img

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { getProjects } from "@/sanity/sanity-utils"
+import Image from "next/image";
 
 
 export default async function Home() {
@@ -6,11 +7,26 @@ export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <div className="font-bold">
-      <div>my projects</div>
-      {projects.map((project) => (
-        <div key={project._id}>{project?.name}</div>
-      ))}
+    <div className="container">
+      <h1 className="text-7xl">hello, i&apos;m <span className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent">chase</span></h1>
+      <div className="grid grid-cols-3 gap-8">
+        {projects.map((project) => (
+
+          <div key={project._id}>
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={250}
+                height={250}
+                className="object-cover rounded lg border border-gray-500"
+              />
+            )}
+            {project.name}
+          </div>
+
+        ))}
+      </div>
     </div>
   );
 
