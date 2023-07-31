d47dbb0f13a45aabdc046a01b77bae91666787ae
chasechristensen1 <chase.christensen@inmoment.com>
2023-07-31 15:43:51 -0600

    error codes

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -19,35 +19,50 @@ type Props = {
 }
 
 export default async function Page({ params }: Props) {
-  const page: PageProps = await getPage(params.slug)
-  
-  return (
-    <div>
-      <Container>
-        <div className="prose dark:prose-invert mx-auto">
-          <header>
-            <h1>{page.subtitle ? page.subtitle : page.title}</h1>
-          </header>
-          <div>
-            {page.content.map((block: Block, index: number) => {
-              if (block._type === 'skills') {
-                return <Skills key={index} /* pass any props you need here */ />
-              }
-
-              return (
-                <PortableText 
-                  key={index} 
-                  value={[block]}
-                  components={{
-                    // Define other custom types if needed
-                  }}
-                />
-              )
-            })}
+  try {
+    const page: PageProps = await getPage(params.slug)
+    
+    if (!page) {
+      // Handle the case where page data could not be fetched
+      // This could be a redirect or a "Page not found" component
+      return <div>Page not found</div>
+    }
+    
+    return (
+      <div>
+        <Container>
+          <div className="prose dark:prose-invert mx-auto">
+            <header>
+              <h1>{page.subtitle ? page.subtitle : page.title}</h1>
+            </header>
+            <div>
+              {page.content.map((block: Block, index: number) => {
+                if (block._type === 'skills') {
+                  return <Skills key={index} /* pass any props you need here */ />
+                }
+    
+                if (block._type === 'block') {
+                  return (
+                    <PortableText 
+                      key={index} 
+                      value={[block]}
+                      components={{
+                        // Define other custom types if needed
+                      }}
+                    />
+                  )
+                }
+                // Handle unrecognized block types
+                return null
+              })}
+            </div>
           </div>
-        </div>
-      </Container>
-    </div>
-  )
+        </Container>
+      </div>
+    )
+  } catch (err) {
+    // Add a basic error handling for any exceptions
+    console.error(err)
+    return <div>An error occurred while loading the page.</div>
+  }
 }
-
