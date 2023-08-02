c157e22a0396cf7b2619cc6ac77ac032ec55e143
chasechristensen1 <chase.christensen@inmoment.com>
2023-08-02 10:47:09 -0600

    page types

--- types/Page.ts
+++ types/Page.ts
@@ -1,11 +1,16 @@
 import { PortableTextBlock } from "sanity";
 
+type Skills = {
+  _type: 'skills';
+  placeholder: string;
+};
+
 export type Page = {
-    name: string;
-    _id: string;
-    _createdAt: Date;
-    title: string;
-    subtitle: string;
-    slug: string;
-    content: PortableTextBlock[];
-}
+  name: string;
+  _id: string;
+  _createdAt: Date;
+  title: string;
+  subtitle: string;
+  slug: string;
+  content: (PortableTextBlock | Skills)[];
+};
