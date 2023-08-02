deb0e4dc3dc6b22c5e370d47ed4fe9c4348c1c9e
chasechristensen1 <chase.christensen@inmoment.com>
2023-08-02 13:36:18 -0600

    clean up sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from 'sanity'
+import {createClient} from '@sanity/client'
 import { deskTool } from "sanity/desk"
-import project from './sanity/schemas/project-schema'
 import schemas from  './sanity/schemas'
 
-export const config = defineConfig ({
+export const config = createClient ({
     projectId: "lgevplo8",
     dataset: "production",
-    title: "Cee App",
     apiVersion: "2023-07-12",
-    basePath: "/studio",
-    plugins: [deskTool()],
-    schema: { types: schemas }
-
 })
