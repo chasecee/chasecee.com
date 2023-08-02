b947ee43ec8c04cbcfb73be7b36dd24725107a6b
chasechristensen1 <chase.christensen@inmoment.com>
2023-08-02 10:43:07 -0600

    next config changes

--- next.config.js
+++ next.config.js
@@ -3,20 +3,20 @@ const nextConfig = {
     images: {
         domains: ['cdn.sanity.io']
     },
-    async headers() {
-        return [
-            {
-                // Apply these headers to all routes in your application.
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'no-store, must-revalidate',
-                    },
-                ],
-            },
-        ]
-    },
+    // async headers() {
+    //     return [
+    //         {
+    //             // Apply these headers to all routes in your application.
+    //             source: '/(.*)',
+    //             headers: [
+    //                 {
+    //                     key: 'Cache-Control',
+    //                     value: 'no-store, must-revalidate',
+    //                 },
+    //             ],
+    //         },
+    //     ]
+    // },
 }
 
 module.exports = nextConfig
