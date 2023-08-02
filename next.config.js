464c25a82e0b190744e571ad0dd88d481ab6dacd
chasechristensen1 <chase.christensen@inmoment.com>
2023-08-02 09:17:23 -0600

    headers fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-		domains: ['cdn.sanity.io']
-	}
+        domains: ['cdn.sanity.io']
+    },
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes in your application.
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, must-revalidate',
+                    },
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
