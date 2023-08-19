26f1ccca64ae5bab171800a3512bf805a6865514
chasechristensen1 <chase.christensen@inmoment.com>
2023-08-18 19:32:08 -0600

    breakpoints

--- app/(site)/components/Header.tsx
+++ app/(site)/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <div className="fixed left-2 right-2 top-2 z-40 md:left-4 md:right-4 md:top-4">
       <header className="header container flex items-center justify-between rounded-full border border-neutral-100/30 bg-neutral-100/30 px-6 py-4 backdrop-blur-md dark:border-neutral-900/30 dark:bg-neutral-900/30">
-        <Breakpoints />
+        {/* <Breakpoints /> */}
 
         <HeaderMenu />
         <Link className="header__title" href="/">
