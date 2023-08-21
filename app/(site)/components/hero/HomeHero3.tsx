ef2b9844674418fb220179b56e0aab2d83b2fdb2
chasechristensen1 <chase.christensen@inmoment.com>
2023-08-21 14:39:49 -0600

    mobile cleanup

--- app/(site)/components/hero/HomeHero3.tsx
+++ app/(site)/components/hero/HomeHero3.tsx
@@ -14,16 +14,16 @@ export default function HomeHero({
 }: HomeHeroProps) {
   return (
     <div
-      className={`my-20 flex flex-col justify-center rounded-xl bg-gradient-radial from-neutral-100 to-neutral-200 px-4 py-24 dark:from-neutral-700  dark:to-neutral-800 lg:h-screen lg:max-h-[80dvh] lg:min-h-[31rem] lg:py-0 ${className}`}
+      className={`my-20 flex flex-col justify-center from-neutral-100 to-neutral-200 py-20 dark:from-neutral-700 dark:to-neutral-800 md:rounded-xl md:bg-gradient-radial md:px-4 md:py-24  lg:mt-24 lg:h-screen lg:max-h-[80dvh] lg:min-h-[31rem] lg:py-0 ${className}`}
     >
       <div
-        className="absolute inset-0 rounded-xl bg-fixed bg-repeat opacity-20"
+        className="absolute inset-0 hidden rounded-xl bg-fixed bg-repeat opacity-20 md:block"
         style={{
           backgroundImage: `url('/noise1.png')`,
           backgroundSize: "200px",
         }}
       ></div>
-      <div className="prose relative mx-auto max-w-none dark:prose-invert xl:max-w-[59rem]">
+      <div className="prose relative mx-auto max-w-none dark:prose-invert lg:max-w-[59rem]">
         <p className="text-xl">{text}</p>
         <h1 className="mb-[2rem] leading-tight tracking-tight sm:text-[4rem] lg:text-[4.7rem]">
           <span className=" block xl:inline">{textB}</span>
