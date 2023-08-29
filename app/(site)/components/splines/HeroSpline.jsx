47214aef76342d9f43b95db132790a4ffb5ddac9
chasechristensen1 <chase.christensen@inmoment.com>
2023-08-29 10:03:38 -0600

    cleanup

--- app/(site)/components/splines/HeroSpline.jsx
+++ app/(site)/components/splines/HeroSpline.jsx
@@ -4,8 +4,8 @@ import Spline from "@splinetool/react-spline";
 export default function App() {
   return (
     <Spline
-      className="absolute inset-0 h-full w-full scale-x-[-100%] opacity-50"
-      scene="https://prod.spline.design/micVrLtLYv44rMgj/scene.splinecode"
+      className="absolute inset-0 h-full w-full opacity-50"
+      scene="https://prod.spline.design/hkdzLhroc-qlROLG/scene.splinecode"
     />
   );
 }
