aacdc6f423b0d06704aca21c578c4149baaccb88
chasechristensen1 <chase.christensen@inmoment.com>
2023-09-02 22:23:57 -0600

    newspline

--- app/(site)/components/splines/HeroSpline.jsx
+++ app/(site)/components/splines/HeroSpline.jsx
@@ -4,8 +4,10 @@ import Spline from "@splinetool/react-spline";
 export default function App() {
   return (
     <Spline
-      className="absolute inset-0 h-full w-full opacity-50"
-      scene="https://prod.spline.design/hkdzLhroc-qlROLG/scene.splinecode"
+      className="absolute inset-0 h-full w-full opacity-100"
+      // scene="https://prod.spline.design/shLi2BeYC8ft0A1N/scene.splinecode"
+      // scene="https://prod.spline.design/hkdzLhroc-qlROLG/scene.splinecode"
+      scene="https://prod.spline.design/vX8EAW7sdrN2unve/scene.splinecode"
     />
   );
 }
